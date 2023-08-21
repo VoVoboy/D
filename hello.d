bae7de23888e370e86fe3b9d52a822651a469572
Bohdan <korogodskijbogdan@gmail.com>
2023-08-21 15:31:22 +0300

    +

--- hello.d
+++ hello.d
@@ -4,7 +4,7 @@ void main() {
     immutable inchesPerFoot = 12;
     immutable cmPerInch = 2.54;
 
-    foreach (feet; 5 .. 7) {
+    foreach (feet; 5 .. 1+feet) {
         writeln(feet);
         // foreach (inches; 0 .. inchesPerFoot) {
         //     writeln(feet, "'", inches, "''\t",
