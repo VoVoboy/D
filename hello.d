61c7299a76189a0ed61133480ff799c53c49d483
Bohdan <korogodskijbogdan@gmail.com>
2023-08-21 15:01:10 +0300

    comments+

--- hello.d
+++ hello.d
@@ -1,4 +1,14 @@
 import std.stdio;
+
 void main() {
-    writeln("Hello, world!", "one more time");
+    immutable inchesPerFoot = 12;
+    immutable cmPerInch = 2.54;
+
+    foreach (feet; 5 .. 7) {
+        writeln(feet);
+        // foreach (inches; 0 .. inchesPerFoot) {
+        //     writeln(feet, "'", inches, "''\t",
+        //         (feet * inchesPerFoot + inches) * cmPerInch);
+        // }
+    }
 }
